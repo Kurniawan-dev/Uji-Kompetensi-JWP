ebe6c50b29f142ba7679d60c21683acb1bd593f1
Kurniawan <kurniawanbrave27@gmail.com>
2024-12-14 15:35:09 +0700

    new hitung luas

---
+++ UjiKompetensi_JWP_Kurniawan/HitungLuasSegitiga/hitungLuasSegitiga.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Hitung Luas Segitiga</h2>
+    <form action="hitung_luas.php" method="POST">
+        <label for="alas">Alas Segitiga:</label><br>
+        <input type="number" id="alas" name="alas" required><br><br>
+
+        <label for="tinggi">Tinggi Segitiga:</label><br>
+        <input type="number" id="tinggi" name="tinggi" required><br><br>
+
+        <input type="submit" value="Hitung">
+    </form>
+</body>
+
+</html>
